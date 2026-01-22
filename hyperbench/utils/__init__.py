a97042c274bce0288ef6e0705fc6bdd8ac2b5147
Tiziano <tizianocitro98@gmail.com>
2026-01-22 12:05:14 +0100

    feat: add utils for hdata tensors

--- hyperbench/utils/__init__.py
+++ hyperbench/utils/__init__.py
@@ -1,5 +1,9 @@
 from .hif import validate_hif_json
+from .torch_utils import empty_edgeattr, empty_edgeindex, to_non_empty_edgeattr
 
 __all__ = [
+    "empty_edgeattr",
+    "empty_edgeindex",
+    "to_non_empty_edgeattr",
     "validate_hif_json",
 ]
---
+++ hyperbench/utils/torch_utils.py
@@ -0,0 +1,16 @@
+import torch
+
+from torch import Tensor
+
+
+def empty_edgeindex() -> Tensor:
+    return torch.empty((2, 0))
+
+
+def empty_edgeattr(num_edges: int) -> Tensor:
+    return torch.empty((num_edges, 0))
+
+
+def to_non_empty_edgeattr(edge_attr: Tensor | None) -> Tensor:
+    num_edges = edge_attr.size(0) if edge_attr is not None else 0
+    return empty_edgeattr(num_edges) if edge_attr is None else edge_attr
