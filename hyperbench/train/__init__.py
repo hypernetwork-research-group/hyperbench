950d76781c54c91373b9e501b843c762e84c9a9a
Tiziano Citro <56075735+tizianocitro@users.noreply.github.com>
2026-02-03 09:47:51 +0100

    feat: add random negative sampler (#26)
    * feat: add random negative sampler
    
    * feat: add unit tests for random negative sampler
    
    * fix: format imports

--- hyperbench/train/__init__.py
+++ hyperbench/train/__init__.py
@@ -1,5 +1,8 @@
+from .negative_sampler import NegativeSampler, RandomNegativeSampler
 from .trainer import MultiModelTrainer
 
 __all__ = [
+    "NegativeSampler",
+    "RandomNegativeSampler",
     "MultiModelTrainer",
 ]
