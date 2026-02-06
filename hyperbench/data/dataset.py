93de258b932eda0362783ea0b7ae88f37fe19d83
ddevin96 <danieledevinco1996@gmail.com>
2026-02-06 11:28:12 +0100

    feat: added datasets - alfabetical order

--- hyperbench/data/dataset.py
+++ hyperbench/data/dataset.py
@@ -30,17 +30,18 @@ class DatasetNames(Enum):
     EMAIL_W3C = "email-W3C"
     GEOMETRY = "geometry"
     GOT = "got"
-    IMBD = "imdb"
+    IMDB = "imdb"
     MUSIC_BLUES_REVIEWS = "music-blues-reviews"
     NBA = "nba"
     NDC_CLASSES = "NDC-classes"
     NDC_SUBSTANCES = "NDC-substances"
+    PATENT = "patent"
+    PUBMED = "pubmed"
     RESTAURANT_REVIEWS = "restaurant-reviews"
     THREADS_ASK_UBUNTU = "threads-ask-ubuntu"
     THREADS_MATH_SX = "threads-math-sx"
     TWITTER = "twitter"
     VEGAS_BARS_REVIEWS = "vegas-bars-reviews"
-    PATENT = "patent"
 
 
 class HIFConverter:
@@ -415,25 +416,89 @@ class AlgebraDataset(Dataset):
     DATASET_NAME = "ALGEBRA"
 
 
-class DBLPDataset(Dataset):
-    DATASET_NAME = "DBLP"
+class AmazonDataset(Dataset):
+    DATASET_NAME = "AMAZON"
 
 
-class ThreadsMathsxDataset(Dataset):
-    DATASET_NAME = "THREADSMATHSX"
+class ContactHighSchoolDataset(Dataset):
+    DATASET_NAME = "CONTACT_HIGH_SCHOOL"
 
 
-class PatentDataset(Dataset):
-    DATASET_NAME = "PATENT"
+class ContactPrimarySchoolDataset(Dataset):
+    DATASET_NAME = "CONTACT_PRIMARY_SCHOOL"
+
+
+class CoraDataset(Dataset):
+    DATASET_NAME = "CORA"
 
 
 class CourseraDataset(Dataset):
     DATASET_NAME = "COURSERA"
 
 
+class DBLPDataset(Dataset):
+    DATASET_NAME = "DBLP"
+
+
+class EmailEnronDataset(Dataset):
+    DATASET_NAME = "EMAIL_ENRON"
+
+
+class EmailW3CDataset(Dataset):
+    DATASET_NAME = "EMAIL_W3C"
+
+
+class GeometryDataset(Dataset):
+    DATASET_NAME = "GEOMETRY"
+
+
+class GOTDataset(Dataset):
+    DATASET_NAME = "GOT"
+
+
 class IMDBDataset(Dataset):
     DATASET_NAME = "IMDB"
 
 
-class CoraDataset(Dataset):
-    DATASET_NAME = "CORA"
+class MusicBluesReviewsDataset(Dataset):
+    DATASET_NAME = "MUSIC_BLUES_REVIEWS"
+
+
+class NBADataset(Dataset):
+    DATASET_NAME = "NBA"
+
+
+class NDCClassesDataset(Dataset):
+    DATASET_NAME = "NDC_CLASSES"
+
+
+class NDCSubstancesDataset(Dataset):
+    DATASET_NAME = "NDC_SUBSTANCES"
+
+
+class PatentDataset(Dataset):
+    DATASET_NAME = "PATENT"
+
+
+class PubmedDataset(Dataset):
+    DATASET_NAME = "PUBMED"
+
+
+class RestaurantReviewsDataset(Dataset):
+    DATASET_NAME = "RESTAURANT_REVIEWS"
+
+
+class ThreadsAskUbuntuDataset(Dataset):
+    DATASET_NAME = "THREADS_ASK_UBUNTU"
+
+
+class ThreadsMathsxDataset(Dataset):
+    DATASET_NAME = "THREADS_MATH_SX"
+
+
+class TwitterDataset(Dataset):
+    DATASET_NAME = "TWITTER"
+
+
+class VegasBarsReviewsDataset(Dataset):
+    DATASET_NAME = "VEGAS_BARS_REVIEWS"
